3b1bf019271ab753e87d12e48d9aec34073d700d
isaac270 <isaacgeorge807@gamil.com>
2026-02-24 22:16:24 +0100

    Your commit gHS

--- api/lamda.php
+++ api/lamda.php
@@ -1,4 +1,5 @@
 <?php
 
 // Forward Vercel requests to normal index.php
-require '/../public/index.php';
+require(dirname(__DIR__) . '/public/index.php');
+// require __DIR__ . '/../public/index.php';
